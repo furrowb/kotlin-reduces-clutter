38a9f9a8e48dc10be3ab94524d8091fa194f5a24
beau.furrow <beau.furrow@gmail.com>
2019-01-23 21:09:34 -0500

    Basic start with constructor and getter/setter.

---
+++ src/main/java/com/java/is/okay/GetAndSet.java
@@ -0,0 +1,29 @@
+package com.java.is.okay;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GetAndSet {
+    private List<String> myList;
+
+    private final Map<Integer, String> myMap;
+
+    public GetAndSet(List<String> myList, Map<Integer, String> myMap) {
+        this.myList = myList;
+        this.myMap = myMap;
+    }
+
+    public List<String> getMyList() {
+        return myList;
+    }
+
+    public void setMyList(List<String> myList) {
+        this.myList = myList;
+    }
+
+    public Map<Integer, String> getMyMap() {
+        return myMap;
+    }
+}
---
+++ src/main/kotlin/com/kotlin/is/awesome/GetAndSet.kt
@@ -0,0 +1,5 @@
+package com.kotlin.`is`.awesome
+
+import java.util.*
+
+class GetAndSet(var myList: List<String>, val myMap: HashMap<Int, String>)
