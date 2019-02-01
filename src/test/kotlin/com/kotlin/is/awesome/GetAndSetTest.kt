98bdba3f68d750459435708ab074ee4975c13500
beau.furrow <beau.furrow@gmail.com>
2019-01-31 21:41:15 -0500

    Added setters to get/set test. More comments.

--- src/test/kotlin/com/kotlin/is/awesome/GetAndSetTest.kt
+++ src/test/kotlin/com/kotlin/is/awesome/GetAndSetTest.kt
@@ -10,6 +10,11 @@ class GetAndSetTest: ShouldSpec() {
             val java = JavaGetAndSet(mutableListOf("Java's", "Alright"), mutableMapOf(1 to "Java"))
             val kotlin = KotlinGetAndSet(mutableListOf("Kotlin's", "Cool"), mapOf(10 to "Kotlin"))
 
+            // Setters
+            java.myList = mutableListOf("New", "List")
+            kotlin.myList = mutableListOf("New", "List")
+
+            // Getters
             java.myMap.shouldContain(1, "Java")
             kotlin.myMap.shouldContain(10, "Kotlin")
         }
