cc3a7ffd32e0478a1e5fac91d5eae6e85e9e13c3
beau.furrow <beau.furrow@gmail.com>
2019-02-09 01:16:59 -0500

    Updated extension function test.

--- src/main/kotlin/com/kotlin/is/awesome/KotlinGetAndSet.kt
+++ src/main/kotlin/com/kotlin/is/awesome/KotlinGetAndSet.kt
@@ -3,4 +3,4 @@ package com.kotlin.`is`.awesome
 // 'var' is equivalent to 'Class myClass'. In the constructor, this means generate a getter/setter
 // 'val' is equivalent to 'final Class myClass'.  In the constructor, this means generate a getter only
 // The 'class KotlinGetAndSet()` is creating a default constructor.
-class KotlinGetAndSet(var myList: List<String>, val myMap: Map<Int, String>)
+class KotlinGetAndSet(var myList: MutableList<String>, val myMap: Map<Int, String>)
--- src/test/kotlin/com/kotlin/is/awesome/ExtensionFunctionTest.kt
+++ src/test/kotlin/com/kotlin/is/awesome/ExtensionFunctionTest.kt
@@ -6,21 +6,37 @@ import io.kotlintest.specs.ShouldSpec
 
 class ExtensionFunctionTest: ShouldSpec() {
     init {
-        should("Use extension functions for GetAndSet classes") {
+        should("Use extension functions for Java GetAndSet class") {
             // Needs to be MutableList so our function can modify it
-            val javaGetAndSet = JavaGetAndSet(mutableListOf("Item1", "Item2"), mapOf())
-
+            val javaGetAndSet = JavaGetAndSet(mutableListOf("Item1", "Item2"), emptyMap())
             javaGetAndSet.myList.size shouldBe 2
+
             // Removes the first element
             javaGetAndSet.myNewFunction()
 
             javaGetAndSet.myList.size shouldBe 1
             javaGetAndSet.myList.first() shouldBe "Item2"
         }
+
+        should("Use extension functions for Kotlin GetAndSet class") {
+            val kotlinGetAndSet = KotlinGetAndSet(mutableListOf("Item1", "Item2"), emptyMap())
+            kotlinGetAndSet.myList.size shouldBe 2
+
+            // Removes the first element
+            kotlinGetAndSet.myNewFunction()
+
+            kotlinGetAndSet.myList.size shouldBe 1
+            kotlinGetAndSet.myList.first() shouldBe "Item2"
+        }
     }
 }
 
 // Adding a new function that does not exist on the original class
 fun JavaGetAndSet.myNewFunction() {
+    this.myList.removeAt(0)
+}
+
+// Adding a new function that does not exist on the original class
+fun KotlinGetAndSet.myNewFunction() {
     this.myList.removeAt(0)
 }
