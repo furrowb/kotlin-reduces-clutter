9cd4ede4c4697178cc8c46d98620a279d9a3d48f
beau.furrow <beau.furrow@gmail.com>
2019-08-21 00:41:26 -0400

    Add annotation for Java lambda

--- src/main/java/com/java/is/okay/lambda/FilterLambda.java
+++ src/main/java/com/java/is/okay/lambda/FilterLambda.java
@@ -3,6 +3,7 @@ package com.java.is.okay.lambda;
 import java.util.List;
 
 // Need to create lambda interface to create lambda
+@FunctionalInterface
 public interface FilterLambda<T> {
 
     // Takes a List of T, returns List of T
